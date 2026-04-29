caf32daa7c69ef578ae58bfc32f41d7053e99209
dileepkdileep039-max <dileepkdileep039@gmail.com>
2026-04-29 21:10:19 +0530

    added student marks assignment

---
+++ main.py
@@ -0,0 +1,11 @@
+marks = [80,75,90]
+total = sum(marks)
+average = total/ len(marks)
+if average >=80:
+  grade = "A"
+else :
+  grade = "B"
+
+print("total:",total)
+print("Average:",average)
+print("Grade:",grade)
